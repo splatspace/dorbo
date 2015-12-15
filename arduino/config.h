5097c2c9fdfe09bc8af260326c627189d5f494c4
Shaw Terwilliger <sterwill@tinfig.com>
2015-12-15 11:20:46 -0500

    Fixed some comment typos; added Pro Micro note

--- arduino/config.h
+++ arduino/config.h
@@ -6,14 +6,21 @@
 // Enables serial debug output
 #define DEBUG
 
+// Hardware Note
+//
+// Dorbo was developed for a SparkFun Pro Micro (5V).  If you want to run it
+// on another board, you'll probably have to change the way the interrupts
+// are defined below.  Newer Arduino libraries include a digitalPinToInterrupt
+// macro/function that you should use instead of INT0, INT1, INT2, etc.
+
 //////////////////////////////////////////////////////////////////////////////
 // Credential Storage
 //////////////////////////////////////////////////////////////////////////////
 
 // Number of Wiegand-26 credentials to store in EEPROM.  Each credential 
-// requires 3 bytes (24-bits; we don't store the 2 parity bits) of EEPROM 
-// and 3 bytes of SRAM (for buffering during load).  Feel free to raise if 
-// you have more memory or lower if you need room for other things.
+// requires 24-bits (3 bytes) of EEPROM.  We don't store the 2 parity bits.
+// Increase the value if you have more memory or lower it if you need room 
+// for other things.
 #define WIEGAND26_MAX_CREDS 100
 
 //////////////////////////////////////////////////////////////////////////////
@@ -21,24 +28,24 @@
 //////////////////////////////////////////////////////////////////////////////
 
 // Defines the number of Wiegand readers handled by this program.  Each reader 
-// requires 2 data pins.  All readers must be connected to pins in a single IO 
-// port on the microcontroller.
+// requires 2 data pins, and each pin must support external interrupts.
 #define NUM_WIEGAND_READERS 2
 
 // Pins connected to readers.  All pins must be capable of triggering
 // external interrupts.  Must be an array initializer of two dimensions
 // [NUM_WIEGAND_READERS][2] where the inner dimension defines 
 // {DATA_0_PIN, DATA_1_PIN}.
-#define WIEGAND_READER_PINS {{1, 0},       {2, 3}}
+#define WIEGAND_READER_PINS {{1, 0}, {2, 3}}
 
 // Interrupts to hook for pins defined at WIEGAND_READER_PINS.  Must
 // be an array initializer of the same size.
 #define WIEGAND_READER_INTS {{INT2, INT3}, {INT1, INT0}}
 
 // Abort the read and reset for a new value if reader input lines are idle 
-// for this many milliseconds).  Prevents noise from accumulating and 
-// spoiling the next read.  Typical inter-pulse spacing for readers is
-// 1 ms, so a short timeout a little longer than this is useful.
+// for this many milliseconds.  Prevents noise from accumulating and 
+// spoiling the next read, which could be minutes or hours after the noise.  
+// Typical inter-pulse spacing for readers is 1 ms, so a timeout a little 
+// longer than this is useful.
 // 
 // If your environment has large amounts of electrical noise you could 
 // try setting this to a smaller value, but don't make it shorter than 
@@ -48,9 +55,9 @@
 // value.  Longer values really won't hurt unless there's a lot of noise
 // in the lines.
 //
-// If you don't have a physical Wiegand reader you can set this to a value
-// to 3000 (3 seconds), connect two normally-closed pushbuttons across the
-// data lines to ground, and manually key in 26 bits.  Yes, this really 
+// If you don't have a physical Wiegand reader you can set this to a long
+// value like 3000 (3 seconds), connect two normally-closed pushbuttons across
+// the data lines to ground, and manually key in 26 bits.  Yes, this really 
 // works.
 //
 // Must be greater than 0 and less than 2^31 (2147483648 ms ~= 24.9 days).
@@ -61,7 +68,8 @@
 //////////////////////////////////////////////////////////////////////////////
 
 // Defines the number of doors controlled by this program.  Each door has one
-// strike controlled by one output pin.
+// strike controlled by one output pin that goes HIGH to open the door, and
+// LOW to close it.
 #define NUM_DOORS 2
 
 // Output pins connected to strikes.  Must be an array initializer of 
@@ -76,8 +84,8 @@
 // array initializer of length NUM_DOORs, each item is an array of 
 // {pin, enabled_state} where enabled_state specifies the state (HIGH or LOW) 
 // that the pin is set to to indicate success (reader devices differ in 
-// their conventions).  Pin 255 specifies no indicator LED is present for 
-// that door.
+// their conventions).  Specify pin 255 with any enabled state to disable 
+// the LED for that door index.
 #define DOOR_ACCEPTED_LED_PINS   {{4, LOW}, {5, LOW}}
 
 //////////////////////////////////////////////////////////////////////////////
